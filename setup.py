3d694b1b1dccb81f097002801338af71ab7c2a14
fhp <fengheping@linkeddt.com>
2025-09-21 17:12:57 +0800

    build

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup, find_packages
+import os
 
-with open("README.md", "r", encoding="utf-8") as fh:
+# Get the long description from the README file
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    install_requires = [line.strip() for line in fh.readlines() if line.strip() and not line.startswith("#")]
+# Read requirements
+install_requires = []
+requirements_path = os.path.join(here, "requirements.txt")
+if os.path.exists(requirements_path):
+    with open(requirements_path, "r", encoding="utf-8") as fh:
+        install_requires = [line.strip() for line in fh.readlines() if line.strip() and not line.startswith("#")]
 
-with open("LICENSE", "r", encoding="utf-8") as fh:
-    license_text = fh.read()
+# Read license
+license_text = ""
+license_path = os.path.join(here, "LICENSE")
+if os.path.exists(license_path):
+    with open(license_path, "r", encoding="utf-8") as fh:
+        license_text = fh.read()
 
 setup(
     name="lsyflasksdkcore",
